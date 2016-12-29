94bf75299222ece79fdc05c7f93c30b80345c680
Edison Mera <efmera@gmail.com>
2016-12-29 14:50:16 +0100

    Added pack.pl

---
+++ pack.pl
@@ -0,0 +1,9 @@
+name('dialect-ciao').
+version('0.0.1').
+title('Ciao Prolog compatibility library').
+keywords([dialect, ciao]).
+author('Edison Mera Menendez', 'http://www.edisonm.com/').
+packager('Edison Mera Menendez', 'http://www.edisonm.com/').
+maintainer('Edison Mera Menendez', 'http://www.edisonm.com/').
+home('https://github.com/edisonm/dialect-ciao').
+download('https://github.com/edisonm/dialect-ciao.git').
